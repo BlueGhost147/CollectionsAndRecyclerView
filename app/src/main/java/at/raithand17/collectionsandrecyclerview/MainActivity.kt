aba26eb2345d5534dbb40b012c0642864f618876
Andreas <andreas.raith@edu.fh-joanneum.at>
2018-12-12 12:11:51 +0100

    Init commit

---
+++ app/src/main/java/at/raithand17/collectionsandrecyclerview/MainActivity.kt
@@ -0,0 +1,12 @@
+package at.raithand17.collectionsandrecyclerview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
